018b55a3d376529e73679d502de612fd29f94f89
Intruder Shanky <intrudershanky@outlook.com>
2016-10-10 12:05:30 +0530

    Attributes Updated

--- app/src/main/java/com/intrusoft/diagonalApp/MainActivity.java
+++ app/src/main/java/com/intrusoft/diagonalApp/MainActivity.java
@@ -1,10 +1,7 @@
 package com.intrusoft.diagonalApp;
 
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.intrusoft.squint.DiagonalView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
